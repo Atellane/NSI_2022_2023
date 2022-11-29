9c75ac95917e5bcc346f235f315838bf2824a509
Atellane <anonymousconcept1@gmail.com>
2022-11-29 20:03:24 +0100

    correction des commentaires

--- python/projet/nombres_de_fermat/main.py
+++ python/projet/nombres_de_fermat/main.py
@@ -1,8 +1,9 @@
 from class_and_methods.nombre_de_fermat import NombreDeFermat
 from time import sleep
 """
-crée une instance 'testNombreFermat' de la class 'NombreDeFermat' avec 'self.indice = 3' 
-et vérifie si l'image de 3 par la fonction qui donne les nombres de Fermat est première ou non
+crée une instance 'testNombreFermat' de la class 'NombreDeFermat' avec 'self.indice = i' 
+et vérifie si l'image de i par la fonction qui donne les nombres de Fermat est première ou non pour
+i ∈ [0 ; 5]
 """
 for i in range(6) :
     testNombreFermat: object = NombreDeFermat(i)
@@ -10,6 +11,10 @@ for i in range(6) :
 
 sleep(5.0)
 
+"""
+crée une instance 'calculNombreFermat' de la class 'NombreDeFermat' avec 'self.indice = i' 
+et calcule l'image de i par la fonction qui donne les nombres de Fermat pour i ∈ [0 ; 5]
+"""
 for i in range(32) :
     calculNombreFermat: object = NombreDeFermat(i)
     print(calculNombreFermat.nombres_de_fermat(), "\n")
