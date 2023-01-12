bdca7eef866c2b0c3a720b658512245c45403df5
Atellane <anonymousconcept1@gmail.com>
2023-01-12 18:44:58 +0100

    fin exercice 4

--- python/exercices/chapitre_4/exercice_4.py
+++ python/exercices/chapitre_4/exercice_4.py
@@ -5,26 +5,36 @@ def validityTestScrabble(stringToTest: str) -> str :
 	compteurVoyelles: int = 0
 	compteurConsonnes: int = 0
 
-	invalidBecauseOfLetters: str = "Votre chaine est invalide, elle ne peut contenir que des lettres :("
-	invalidBecauseOfLength: str = "Votre chaine est invalide, elle doit contenir au moins 7 lettres ;)"
-	invalidBecauseOfVoyellesOrConsonnes: str = "Votre chaine est invalide, elle doit contenir au moins 2 consonnes et 2 voyelles :/"
-	Valid = "Votre chaine est valide :)"
+	invalidBecauseOfLetters: str = f"Votre chaine {stringToTest} est invalide, elle ne peut contenir que des lettres :("
+	invalidBecauseOfLength: str = f"Votre chaine {stringToTest} est invalide, elle doit contenir au moins 7 lettres ;)"
+	invalidBecauseOfVoyellesOrConsonnes: str = f"Votre chaine {stringToTest} est invalide, elle doit contenir au moins 2 consonnes et 2 voyelles :/"
+	valid: str = f"Votre chaine {stringToTest} est valide :)"
 
-	if len(stringToTest) >= 7:
+	if len(stringToTest) >= 7 :
 		for i in stringToTest :
 			try :
 				int(i)
 				return invalidBecauseOfLetters
 			except ValueError :
-				if i in voyelles:
-					compteurVoyelles += 1
-				else:
-					compteurConsonnes += 1
-				if compteurVoyelles < 2 or compteurConsonnes < 2:
-					return invalidBecauseOfVoyellesOrConsonnes
-				continue
-		return
+				for j in voyelles :
+					if i == j:
+						compteurVoyelles += 1
+				for j in consonnes:
+					if i == j:
+						compteurConsonnes += 1
+		if compteurVoyelles < 2 or compteurConsonnes < 2 :
+			return invalidBecauseOfVoyellesOrConsonnes
+		else:
+			return valid
 	else:
 		return invalidBecauseOfLength
 
- 
+assert validityTestScrabble("12345678") == "Votre chaine 12345678 est invalide, elle ne peut contenir que des lettres :(", "l'erreur empéchant d'utiliser des lettres ne fonctionne pas"
+assert validityTestScrabble("azerty") == "Votre chaine azerty est invalide, elle doit contenir au moins 7 lettres ;)", "l'erreur exigeant 7 lettres ne fonctionne pas"
+assert validityTestScrabble("pqrstuvwx") == "Votre chaine pqrstuvwx est invalide, elle doit contenir au moins 2 consonnes et 2 voyelles :/", "l'erreur exigeant 2 consonnes et 2 voyelles ne fonctionne pas"
+
+longueurMot = randint(7, 20)
+mot = ""
+for i in range(1,longueurMot):
+	mot = mot + chr(randint(65,90))
+print(validityTestScrabble(mot))
